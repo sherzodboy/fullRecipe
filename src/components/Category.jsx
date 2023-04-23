d41335e16a36207e220fe0b5916d47c11a90d824
sherzodboy <kholmamatovsh09@gmail.com>
2023-04-24 01:09:28 +0500

    Category

---
+++ src/components/Category.jsx
@@ -0,0 +1,35 @@
+import { FaPizzaSlice, FaHamburger } from "react-icons/fa";
+import { GiNoodles, GiChopsticks } from "react-icons/gi";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+function Category() {
+  return (
+    <List>
+      <NavLink to={"/cuisine/Italian"}>
+        <FaPizzaSlice />
+        <h4>Italian</h4>
+      </NavLink>
+      <NavLink to={"/cuisine/American"}>
+        <FaHamburger />
+        <h4>American</h4>
+      </NavLink>
+      <NavLink to={"/cuisine/Thai"}>
+        <GiNoodles />
+        <h4>Thai</h4>
+      </NavLink>
+      <NavLink to={"/cuisine/Japanese"}>
+        <GiChopsticks />
+        <h4>Japanese</h4>
+      </NavLink>
+    </List>
+  );
+}
+
+const List = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0rem;
+`;
+
+export default Category;
